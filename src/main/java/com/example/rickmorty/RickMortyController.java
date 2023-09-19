e99b41bc0fdfc3356f2bab981ca6ed788afcdd39
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-19 10:57:40 +0200

    init

---
+++ src/main/java/com/example/rickmorty/RickMortyController.java
@@ -0,0 +1,16 @@
+package com.example.rickmorty;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class RickMortyController {
+
+
+
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        System.out.println("Du hast die testGetAllProducts() Methode über MockMvc aufgerufen ");
+        return productService.findAllProducts();
+    }
+}
